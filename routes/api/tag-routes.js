76e1a5fbd6b0618b89c7f5cce91fb2418ae0ce67
MMVHome <connortsullivan1@gmail.com>
2021-07-12 23:43:33 -0500

    now a thing!

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,127 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+    try 
+    {
+        const tData = await Tag.findAll(
+        {
+            include: 
+            [
+                { 
+                    model: Product, through: ProductTag,
+                }
+            ]
+        });
+
+        res.status(200).json(tData);
+    } 
+    catch(err) 
+    {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+
+    try 
+    {
+        const tData = await Tag.findByPk(req.params.id, 
+        {
+            include: 
+            [
+                { 
+                    model: Product, through: ProductTag,
+                }
+            ]
+        });
+
+        if(!tData) 
+        {
+            res.status(404).json({ message: 'oops nothing here' });
+        }
+        else
+        {
+            res.status(200).json(tData);
+        }
+    } 
+    catch(err) 
+    {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/', async (req, res) => {
+  // create a new tag
+    try 
+    {
+        const tData = await Tag.create(req.body);
+        res.status(200).json(tData);
+    } 
+    catch(err) 
+    {
+        res.status(400).json(err);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a tag's name by its `id` value
+    try 
+    {
+        const tData = await Tag.update(req.body, 
+        {
+            where: 
+            { 
+                id: req.params.id 
+            }
+        });
+        
+        if(!tData) 
+        {
+            res.status(404).json({ message: 'oops nothing here' });
+        }
+        else
+        {
+            res.status(200).json(tData);
+        }
+    } 
+    catch(err) 
+    {
+        res.status(500).json(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete on tag by its `id` value
+    try 
+    {
+        const tData = await Tag.destroy(
+        {
+            where: 
+            { 
+                id: req.params.id 
+            }
+        });
+        
+        if(!tData) 
+        {
+            res.status(404).json({ message: 'oops nothing here' });
+        }
+        else
+        {
+            res.status(200).json(tData);
+        }
+    } 
+    catch(err) 
+    {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
